8266947915771684dfd3bd0db2a3483433d7928c
Faisal Khalid <fkhalid2008@kellogg.northwestern.edu>
2015-09-26 17:03:13 +0100

    changes to search algorithm &main page layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,9 @@ class Post < ActiveRecord::Base
 
    validates :name, presence: true
    validates :city, presence: true
+   validates :address, presence: true
 
    def self.search(query)
-     where("name like ? OR city like ?", "%#{query}%", "%#{query}%")
+     where("name like ? OR city like ? OR address like ?", "%#{query}%", "%#{query}%", "%#{query}%")
        end
 end
